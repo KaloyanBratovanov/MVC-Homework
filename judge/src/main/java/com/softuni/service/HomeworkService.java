de2edae0ade8497352ddbfb074c196e6210e07ae
Kaloyan <kaloyan.i.bratovanov@gmail.com>
2021-03-25 23:57:00 +0200

    ready with profile page end...

--- judge/src/main/java/com/softuni/service/HomeworkService.java
+++ judge/src/main/java/com/softuni/service/HomeworkService.java
@@ -1,4 +1,8 @@
 package com.softuni.service;
 
+import com.softuni.model.service.HomeworkServiceModel;
+
 public interface HomeworkService {
+
+    HomeworkServiceModel findHomeworkForScoring();
 }
--- judge/src/main/java/com/softuni/service/impl/HomeworkServiceImpl.java
+++ judge/src/main/java/com/softuni/service/impl/HomeworkServiceImpl.java
@@ -1,8 +1,28 @@
 package com.softuni.service.impl;
 
+import com.softuni.model.service.HomeworkServiceModel;
+import com.softuni.repository.HomeWorkRepository;
 import com.softuni.service.HomeworkService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HomeworkServiceImpl implements HomeworkService {
+
+    private final HomeWorkRepository homeWorkRepository;
+    private final ModelMapper modelMapper;
+
+    public HomeworkServiceImpl(HomeWorkRepository homeWorkRepository, ModelMapper modelMapper) {
+        this.homeWorkRepository = homeWorkRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public HomeworkServiceModel findHomeworkForScoring() {
+
+        return homeWorkRepository.findTop1ByOrderByComments()
+                .map(homework -> modelMapper
+                .map(homework, HomeworkServiceModel.class)).orElse(null);
+    }
 }
